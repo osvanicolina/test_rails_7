f4c8f4d5f86627c73d471d4370ef2046ad4dc9a4
Osvani <osvacolijim@gmail.com>
2023-09-08 19:19:33 -0400

    Create user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_05_031118) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_08_231502) do
   create_table "articles", force: :cascade do |t|
     t.string "title", null: false
     t.string "description", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username", null: false
+    t.string "email", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
