2fdacf92533897fc231e1ba75f2a149fb183ae46
Osvani <osvacolijim@gmail.com>
2023-09-08 19:49:31 -0400

    Add user validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_08_231502) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_08_233438) do
   create_table "articles", force: :cascade do |t|
     t.string "title", null: false
     t.string "description", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -25,4 +26,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_08_231502) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "articles", "users"
 end
