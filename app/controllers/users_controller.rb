002790bc8e1a1791bc6fa0f1cb9632d347505496
Osvani <osvacolijim@gmail.com>
2023-09-09 09:42:05 -0400

    Clean layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
     before_action :set_user, only: [:show, :edit, :update, :destroy]
 
+    def index
+        @users = User.all
+    end
+
     def show
         @articles = @user.articles 
     end
@@ -25,7 +29,7 @@ class UsersController < ApplicationController
     def update
         if @user.update(user_params)
             flash[:notice] = "Your account information was successfully updated."
-            redirect_to articles_path
+            redirect_to @user
         else
             render 'edit'
         end
