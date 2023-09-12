6e17db00d6b15cdda59999d4b62faa811b644bc5
Osvani <osvacolijim@gmail.com>
2023-09-11 21:26:36 -0400

    Add admin column to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_09_000744) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_12_012333) do
   create_table "articles", force: :cascade do |t|
     t.string "title", null: false
     t.string "description", null: false
@@ -25,6 +25,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_09_000744) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
   add_foreign_key "articles", "users"
