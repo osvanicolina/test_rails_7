9d0f05da72b1b6708630bfd7b80b5494a78927f3
Osvani <osvacolijim@gmail.com>
2024-01-02 20:50:11 -0400

    Many to many relation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_02_150251) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_03_004251) do
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
+
   create_table "articles", force: :cascade do |t|
     t.string "title", null: false
     t.string "description", null: false
